a83922e23b92ebbdf5986987557c8b9ddfb61096
Fatur Rahman <101609961+fatrrr@users.noreply.github.com>
2022-03-22 13:26:22 +0800

    Update PRAK103.kt

--- modul1/PRAK103.kt
+++ modul1/PRAK103.kt
@@ -1,15 +1,17 @@
-import kotlin.math.pow
+import java.util.Scanner
 
 fun main(){
+    val reader3 = Scanner(System.`in`)
     print("Input bilangan = ")
-    val power: Int = readln().toInt()
+    val power: Int = reader3.nextInt()
     var b = 0
-    for(i in 1..50){
+    var i = 2
+    do{
         val temp1 = power*i
+        i++
         if((temp1 % 2 == 0) || (temp1 % 3 == 0)){
             print("$temp1 ")
             b++
         }
-        if(b == 5){break}
-    }
+    }while (b < 5)
 }
